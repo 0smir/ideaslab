a4148b8d8d38f7becc565ba19447d9d9aa18ab2a
Olga Smirnova <koshechka_ma@mail.ru>
2016-03-07 20:29:51 +0200

    add slider animated

---
+++ javascript/slider/primitiv/slider_primitiv.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	
+	
+
+	$('#slider').find('> ul').animate({'margin-left':'-700px'}, 100)
+					.delay(2000)
+					.animate({'margin-left':'-1400px'}, 100)
+					.delay(2000)
+					.animate({'margin-left':'-2100px'}, 100)
+					.delay(2000)
+					.animate({'margin-left':'-2800px'}, 100)
+					.delay(2000)
+					.animate({'margin-left':'-3500px'}, 100)
+					.delay(2000)
+					.animate({'margin-left':'-4200px'}, 100)
+					.delay(2000);
+
+
+
+
+
+});
